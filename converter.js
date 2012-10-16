f62c2bb1f08c6004b48c9e6e6a7772bd7b875337
sobol@webkate.com <sobol@webkate.com>
2012-10-16 09:20:11 +0300

    39 add converter

---
+++ converter.js
@@ -0,0 +1,26 @@
+//require section:
+var fs = require('fs');
+//constant values section:
+var path = "currentServersList";
+
+function convert(array){
+	var arrayForJson = new Array();
+	for (var i = 0; i < array.length; i++) {
+		var tmp = array[i];
+		if (tmp){ 
+			var objToWrite = {
+		  		serverName : tmp.serverName,
+		  		status: tmp.status,
+		  		money: tmp.money,
+		  		rank: tmp.rank
+		  	};		
+  		  	arrayForJson[i] = objToWrite;
+  		} else {
+  			arrayForJson[i] = null;
+  		}
+	}
+	return JSON.stringify(arrayForJson);
+	
+}
+
+exports.convert = convert;
